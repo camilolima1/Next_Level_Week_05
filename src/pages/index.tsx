6888c6b6d33b7836ca4e83d7bde58a25cbed0cde
Camilo Lima <caamilolima@gmail.com>
2021-04-21 20:54:35 -0300

    2 day: creation components header and player and add fake server

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react"
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+    
+  )
+}
+
+//SSR Carrega sempre que o cliente acessa
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes');
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
